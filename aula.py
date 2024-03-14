73ee30072f121ac1963d76060335716ccef1d925
D1DIS <156555605+D1DIS@users.noreply.github.com>
2024-03-13 23:09:14 -0300

    lista aleatoria

--- aula.py
+++ aula.py
@@ -97,3 +97,13 @@ lista = [a1, a2, a3, a4]
 
 escolhido =  random.choice(lista)
 print ('o aluno escolhido foi {}'.format(escolhido))
+
+a1 = input ('digite o nome do primeiro aluno: ')
+a2 = input('digite o nome do segundo aluno: ')
+a3 = input('digite o nome do terceiro aluno: ')
+a4 = input('digite o nome do quarto aluno: ')
+
+random.shuffle(lista)
+print('a ordem de apresentação sera: ')
+print (lista)
+
